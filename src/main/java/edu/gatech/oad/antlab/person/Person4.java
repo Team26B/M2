c29fc3608dd99072e51f6778cbbf606c083b61df
samanthadaugherty <sedaugherty6@gmail.com>
2019-01-24 17:41:28 -0500

    Person4 method written

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -29,10 +29,17 @@ public class Person4 {
      * @param input the string to be modified
      * @return the modified string
      */
+
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+    //Person 4 put your implementation here
+    String out = "";
+    for (int i = 0; i < input.length(); i++) {
+      int asc = (int)input.charAt(i);
+      ++asc;
+      out = out + (char)asc;
     }
+    return out;
+  }
     
     /**
      * Return a string rep of this object
